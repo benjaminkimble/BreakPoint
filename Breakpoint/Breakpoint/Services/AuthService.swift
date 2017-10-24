e731327c081c4c8123c6606f63c4f6660a6ce8ab
Benjamin Kimble <benjaminroykimble@gmail.com>
2017-10-24 04:50:27 -0600

    Created Login and Registration Functions
    - Added account creation functionality to AuthService
    - Added login functionality to AuthService

--- Breakpoint/Breakpoint/Services/AuthService.swift
+++ Breakpoint/Breakpoint/Services/AuthService.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import Firebase
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    //My Functions
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping CompletionHandlerBool) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID,
+                            "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping CompletionHandlerBool) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            
+            loginComplete(true, nil)
+        }
+    }
+}
---
+++ Breakpoint/Breakpoint/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Breakpoint
+//
+//  Created by Benjamin Kimble on 10/24/17.
+//  Copyright © 2017 Benjamin Kimble. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandlerBool = (_ status: Bool, _ error: Error?) -> ()
